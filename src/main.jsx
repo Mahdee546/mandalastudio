66024f8974201778ae2e1d2b8f0c8612dd9e50ef
Mahdee546 <mahdee546@example.com>
2026-09-13 16:22:26 +0530

    Refactor: Revert PWA to normal webpage to prevent caching issues

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Unregister any existing service workers from the PWA plugin
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
